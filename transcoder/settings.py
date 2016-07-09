6bd0d2dec608ae425af5c9bcef08955bffa02d10
martastain <martas@imm.cz>
2016-07-09 04:51:09 +0000

    updated settings

--- transcoder/settings.py
+++ transcoder/settings.py
@@ -1,15 +1,30 @@
+#
+# settings.py
+#
+# function get_settings(**kwargs)
+# Creates transcoder setting dict. Accepts custom params loaded from config file
+#
+
 def get_settings(**kwargs):
     return {
             # Frame settings
 
-            "frame_rate" : int(kwargs.get("frame_rate", 25)),
-            "pixel_format" : kwargs.get("pixel_format", "yuv420p"),
             "width" : int(kwargs.get("width", 720)),
             "height" : int(kwargs.get("height", 576)),
-            "logo_16_9" : kwargs.get("logo_16_9", False),
-            "logo_4_3" : kwargs.get("logo_4_3", False),
-            "expand_levels" : kwargs.get("expand_levels", "auto"),
+            "frame_rate" : int(kwargs.get("frame_rate", 25)),
+            "pixel_format" : kwargs.get("pixel_format", "yuv420p"),
+            "expand_levels" : kwargs.get("expand_levels", "auto"),  # Expand levels from broadcast to full (pc) using levels
             "deinterlace" : kwargs.get("deinterlace", True),
+
+            # Burn-in options
+
+            # Both logos must be specified in order to enable watermarking
+            "logo_4_3" : kwargs.get("logo_4_3", False),
+            "logo_16_9" : kwargs.get("logo_16_9", False),
+
+            "tc_show" : kwargs.get("tc_show", True),
+            "tc_font" : kwargs.get("tc_font", "support/RobotoMono-Medium.ttf"),
+            "tc_size" : kwargs.get("tc_size", 24),
 
             # Video encoder settings
 
@@ -19,6 +34,7 @@ def get_settings(**kwargs):
             "x264_profile" : kwargs.get("x264_profile", "main"),
             "x264_preset" : kwargs.get("x264_preset", "medium"),
             "x264_level" : kwargs.get("x264_level", "4.0"),
+            "gop_size" : kwargs.get("gop_size", 50),
 
             # Audio encoder settings
 
