ba7e657bfe9b45e6f4bc4accb43671c508652490
Martastain <martas@imm.cz>
2016-09-01 19:26:36 +0200

    increased default qtime

--- transcoder/settings.py
+++ transcoder/settings.py
@@ -46,7 +46,7 @@ def get_settings(**kwargs):
 
             "workers" : int(kwargs.get("workers", 1)),
             "threads_per_worker" : kwargs.get("threads_per_worker", "auto"),
-            "qtime" : int(kwargs.get("qtime", 10)),
+            "qtime" : int(kwargs.get("qtime", 20)),
             "loop_delay" : int(kwargs.get("loop_delay", 5)),
             "source_exts" : kwargs.get("source_exts", ["mxf"]),
             "is_fixing" : kwargs.get("is_fixing", False)
