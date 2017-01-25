6a1053c649e9cc43bfc82f5cbbf90d4d481e3d0e
Martastain <martas@imm.cz>
2017-01-25 17:41:41 +0100

    broker update

--- smartcoder/jobs_broker.py
+++ smartcoder/jobs_broker.py
@@ -6,41 +6,40 @@ from nxtools import *
 from .common import *
 from .db import *
 from .assets import *
-from .actions import *
+from .actions import actions
+from .jobs import Job
 
 __all__ = ["JobsBroker"]
 
-
 class JobsBroker(object):
     def __init__(self):
-        self.broker_cache = {}
         thread.start_new_thread(self.work, ())
 
     def is_working(self):
         return time.time() - self.last_seen < 1800
 
     def work(self):
+        if not actions:
+            logging.warning("No action defined. Stopping JobsBroker")
+            return
         while True:
             self.last_seen = time.time()
             self.main()
             time.sleep(5)
 
     def main(self):
-        actions = []
         db = DB()
-        db.query("SELECT id, title, settings FROM actions")
-        for id, title, settings in db.fetchall():
-            actions.append(Action(id, title, settings, db=db))
-
-        if not actions:
-            return
-
         for asset in get_assets(db=db):
-            created_actions = self.broker_cache.get(asset.id, [])
-
             for id_action in actions:
-                if action.id in created_actions:
+                action = actions[id_action]
+                if not action.should_allow(asset):
                     continue
 
-#            if job exists
-#                self.broker_cache
+                if action.should_create(asset):
+                    if asset.has_job(action.id):
+                        continue
+
+                    logging.info("JobsBroker: Creating job {} for {}".format(asset, action))
+                    job = Job(asset.id, action.id, db=db)
+                    job.save()
+
